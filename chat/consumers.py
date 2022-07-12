f1d929eddae61aebe43f4b0935a3e6413aefc648
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-12 11:27:09 +0900

    완성

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,13 +1,21 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_group_name='test'
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
         self.accept()
-        self.send(text_data=json.dumps({
-            'type':'connection_established',
-            'message':'You are now connected!'
-        }))
+        # self.send(text_data=json.dumps({
+        #     'type':'connection_established',
+        #     'message':'You are now connected!'
+        # }))
     
     def receive(self, text_data):
         text_data_json =json.loads(text_data)
@@ -15,7 +23,23 @@ class ChatConsumer(WebsocketConsumer):
         print('message:', message)
         
         ##다시 클라이언트에게 보내줌
-        self. send(text_data=json.dumps({
+        # self. send(text_data=json.dumps({
+        #     'type':'chat',
+        #     'message':message
+        # }))
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message':message
+            }
+        )
+    
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
             'type':'chat',
             'message':message
         }))
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -43,6 +43,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'mywebsite.asgi.application' #4분 21초까지
 
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
